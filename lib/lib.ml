c826ae46df6556dafd51b55dbc4a2a9ba43924ac
Viacheslav <vaceslavkozin619@gmail.com>
2023-07-27 11:59:00 +0200

    name refactoring

--- lib/lib.ml
+++ lib/lib.ml
@@ -48,14 +48,12 @@ let wrap_expr loc expr =
                          ]
   )
 
-
-let wrap_let loc b =
-  let new_expr = wrap_expr loc b.pvb_expr in
+let expr_to_str_item bind expr =
   {
-    pvb_pat = b.pvb_pat;
-    pvb_expr = new_expr;
-    pvb_attributes = b.pvb_attributes;
-    pvb_loc = b.pvb_loc;
+    pvb_pat = bind.pvb_pat;
+    pvb_expr = expr;
+    pvb_attributes = bind.pvb_attributes;
+    pvb_loc = bind.pvb_loc;
   } 
 
 let wrap_let_bind loc bind =
@@ -72,16 +70,10 @@ let wrap_let_bind loc bind =
     | _ -> wrap_expr loc expr
   in
 
-  let new_expr = find_wrap bind.pvb_expr in
-  {
-    pvb_pat = bind.pvb_pat;
-    pvb_expr = new_expr;
-    pvb_attributes = bind.pvb_attributes;
-    pvb_loc = bind.pvb_loc;
-  } 
+  find_wrap bind.pvb_expr
+  |> expr_to_str_item bind
 
-
-let ext_fun ~ctxt arr =
+let ext_let_expander ~ctxt arr =
   let loc = Expansion_context.Extension.extension_point_loc ctxt in
   let expr_desc = arr.pexp_desc in
   match expr_desc with
@@ -94,10 +86,7 @@ let ext_fun ~ctxt arr =
   | _ ->
      Location.raise_errorf ~loc "Extension applies only to let bindings."
 
-(* let extracter () = *)
-  (* Ast_pattern.(ptop_def __) *)
-
-let top_ext ~ctxt arr =
+let top_def_expander ~ctxt arr =
   let loc = Expansion_context.Extension.extension_point_loc ctxt in
   match arr with
   | PStr str ->
@@ -113,33 +102,26 @@ let top_ext ~ctxt arr =
         }
      | _ -> Location.raise_errorf
               ~loc "Extension applies only to let bindings." end
-  (* | PPat (pat, expr) -> *)
-     (* match expr with *)
-     (* | Some expr ->  *)
-        (* { *)
-          (* pstr_desc = PPat(pat, expr); *)
-          (* pstr_loc = st_item.pstr_loc; *)
-        (* } *)
   | _ ->
      Location.raise_errorf ~loc "Extension applies only to let bindings."
 
 
-let exten = Extension.V3.declare
+let exp_let_ext = Extension.V3.declare
               "catch"
               Extension.Context.Expression
               Ast_pattern.(single_expr_payload __)
-              ext_fun
+              ext_let_expander
 
-let top = Extension.V3.declare
+let top_def_ext = Extension.V3.declare
             "catch"
             Extension.Context.Structure_item
             Ast_pattern.__
-            top_ext
+            top_def_expander
 
-let rule = Context_free.Rule.extension exten
-let top_rule = Context_free.Rule.extension top
+let exp_let_rule = Context_free.Rule.extension exp_let_ext
+let top_def_rule = Context_free.Rule.extension top_def_ext
 
 let () = 
-  Driver.register_transformation ~rules:[rule] "wrap";
-  Driver.register_transformation ~rules:[top_rule] "catch";
+  Driver.register_transformation ~rules:[exp_let_rule] "wrap";
+  Driver.register_transformation ~rules:[top_def_rule] "catch";
   ()
